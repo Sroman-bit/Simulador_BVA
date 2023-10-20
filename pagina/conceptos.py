05e427d5067e2a25c2c8b89698535803c617e211
Sroman-bit <129084537+Sroman-bit@users.noreply.github.com>
2023-10-20 14:06:14 -0500

    Update and rename Portfolio.py to conceptos.py

---
+++ pagina/conceptos.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Conceptos Basicos")
+
+st.write(" ¿Que es un Ticker?")
+st.write("Es un código alfanumérico que indica los valores de un negocio que cotiza en una determinada bolsa de valores. Las empresas que cotizan en el mercado Nasdaq tienen tickers de cuatro letras, mientras que las que cotizan en la Bolsa de Nueva York tienen tickers de tres letras.
+")
