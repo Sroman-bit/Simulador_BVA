d6e79a0352a6008b5653e4fcb3baca3cef87615b
Sroman-bit <129084537+Sroman-bit@users.noreply.github.com>
2023-10-28 22:22:19 -0500

    Update 📖_Conceptos_Basicos.py

--- pages/📖_Conceptos_Basicos.py
+++ pages/📖_Conceptos_Basicos.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.title("Conceptos Basicos")
 
 st.write(" ¿Que es un Ticker?")
-st.write(" ")
 st.write("Es un código alfanumérico que indica los valores de un negocio que cotiza en una determinada bolsa de valores. Las empresas que cotizan en el mercado Nasdaq tienen tickers de cuatro letras, mientras que las que cotizan en la Bolsa de Nueva York tienen tickers de tres letras.")
 st.write(" ")
 st.write(" ¿Que son los Activos Financieros?")
